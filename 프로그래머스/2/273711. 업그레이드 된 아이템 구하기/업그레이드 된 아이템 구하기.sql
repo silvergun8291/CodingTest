540a319de2419b8f9cfb94188ac8021a2377af5a
silvergun8291 <81484874+silvergun8291@users.noreply.github.com>
2024-07-18 23:24:09 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -0,0 +1,19 @@
+SELECT
+    INFO.ITEM_ID,
+    INFO.ITEM_NAME,
+    INFO.RARITY
+FROM
+    ITEM_INFO AS INFO
+JOIN
+    ITEM_TREE AS TREE ON INFO.ITEM_ID = TREE.ITEM_ID
+WHERE
+    PARENT_ITEM_ID IN (
+        SELECT
+            ITEM_ID
+        FROM
+            ITEM_INFO
+        WHERE
+            RARITY = 'RARE'
+    )
+ORDER BY
+    ITEM_ID DESC;
