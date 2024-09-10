87f25e4e3df73ba96b12c34fac35c758e635a51b
silvergun8291 <81484874+silvergun8291@users.noreply.github.com>
2024-09-11 00:34:11 +0900

    [level 2] Title: 전화번호 목록, Time: 93.54 ms, Memory: 30.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,15 +1,10 @@
 from collections import Counter
 
 def solution(phone_book):
-    counter = Counter(phone_book)
+    phone_book.sort()
 
-    for phone_number in phone_book:
-        target = ""
-        
-        for number in phone_number:
-            target += number
-            
-            if target in counter and target != phone_number:
-                return False
-    
+    for i in range(len(phone_book) - 1):
+        if phone_book[i + 1].startswith(phone_book[i]):
+            return False
+
     return True
