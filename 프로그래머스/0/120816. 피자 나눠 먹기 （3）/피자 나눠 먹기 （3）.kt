ba2a7759c4a18ed8006f5f44903a70a317f1b18f
silvergun8291 <81484874+silvergun8291@users.noreply.github.com>
2024-12-08 03:29:24 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.02 ms, Memory: 62.7 MB -BaekjoonHub

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.kt
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.kt
@@ -1,14 +1,6 @@
 class Solution {
     fun solution(slice: Int, n: Int): Int {
         var answer: Int = 0
-        
-        if (n % slice > 0) {
-            answer = n / slice + 1
-        }
-        else {
-            answer = n / slice
-        }
-        
         return answer
     }
 }
