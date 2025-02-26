2411b73f980e73c3999c45a613a6b71ceae0c69b
silvergun8291 <81484874+silvergun8291@users.noreply.github.com>
2025-02-26 19:53:03 +0900

    [Silver V] Title: 색종이, Time: 104 ms, Memory: 14032 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,62 +1,33 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
-/**
- * 아이디어 : 시간 제한이 1초 이기 때문에, 색종이가 붙은 구역을 check 표시하고 check 표시 개수를 세는 방식을 사용
- * 시간 :
- * 메모리 :
- * 난이도 : 하 (시간 복잡도가 널널하고 구현 아이디어가 금방 떠올라서 쉽게 해결할 수 있었습니다.)
- */
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int N;   // 색종이 개수
-    static int colorSize = 10;  // 색종이의 크기
-    static int whiteSize = 100; // 도화지 사이즈
-    static int [][] board = new int[whiteSize][whiteSize];  // 도화지
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-    // x, y 좌표가 주어지면 해당 좌표에 색종이를 붙일 함수
-    static void mark(int x, int y) {
-        // board 배열에서 해당 영역에 모두 true 값을 대입
-        for (int i = y; i < y + colorSize; i++) {
-            for (int j = x; j < x + colorSize; j++) {
-                board[i][j] = 1;
-            }
-        }
-    }
+        boolean [][] board = new boolean[100][100];
 
-    static int countMark() {
-        int count = 0;
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < whiteSize; i++) {
-            for (int j = 0; j < whiteSize; j++) {
-                if (board[i][j] == 1) {
-                    count++;
+            for (int j = a; j < a + 10; j++) {
+                for (int k = b; k < b + 10; k++) {
+                    board[j][k] = true;
                 }
             }
         }
 
-        return count;
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        // 색종이의 개수를 입력받음
-        N = Integer.parseInt(br.readLine());
-        int area = 0;
-
-        // 색종이의 좌표를 입력받아 도화지를 채움
-        for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());   // 색종이 x 좌표
-            int y = Integer.parseInt(st.nextToken());   // 색종이 y 좌표
-
-            mark(x, y); // 도화지에 색종이 영역을 채움
+        int count = 0;
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                if (board[i][j])
+                    count++;
+            }
         }
 
-        area = countMark(); // 도화지에 색종이 영역을 카운트
-        System.out.println(area);   // 색종이 영역의 넓이를 출력
+        System.out.println(count);
     }
 }
