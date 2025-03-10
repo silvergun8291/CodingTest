374048fe42647d0b62dae74670c95bd3a104963c
silvergun8291 <81484874+silvergun8291@users.noreply.github.com>
2025-03-10 22:16:13 +0900

    [Silver I] Title: 숨바꼭질, Time: 144 ms, Memory: 20544 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -2,41 +2,46 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static final int MAX = 100000;
+    static int[] visited;
+    static int minTime;
+    static int target;
+
+    static void bfs(int position) {
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.offer(position);
+        visited[position] = 0;
+
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+
+            if (cur == target) {
+                minTime = visited[cur];
+                return;
+            }
+
+            int [] next = {cur-1, cur+1, cur*2};
+
+            for (int i : next) {
+                if (i >= 0 && i < 100001 && visited[i] == -1) {
+                    visited[i] = visited[cur] + 1;
+                    queue.offer(i);
+                }
+            }
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+        target = Integer.parseInt(st.nextToken());
 
-        System.out.println(bfs(N, K));
-    }
+        visited = new int[100001];
+        Arrays.fill(visited, -1);
+        minTime = Integer.MAX_VALUE;
 
-    static int bfs(int start, int target) {
-        if (start == target) return 0;
-
-        Queue<Integer> queue = new LinkedList<>();
-        boolean[] visited = new boolean[MAX + 1];
-        int[] time = new int[MAX + 1];
-
-        queue.offer(start);
-        visited[start] = true;
-
-        while (!queue.isEmpty()) {
-            int current = queue.poll();
-
-            for (int next : new int[]{current - 1, current + 1, current * 2}) {
-                if (next == target) return time[current] + 1;
-
-                if (next >= 0 && next <= MAX && !visited[next]) {
-                    queue.offer(next);
-                    visited[next] = true;
-                    time[next] = time[current] + 1;
-                }
-            }
-        }
-
-        return -1; // 도달할 수 없는 경우 (실제로는 발생하지 않음)
+        bfs(N);
+        System.out.println(minTime);
     }
 }
