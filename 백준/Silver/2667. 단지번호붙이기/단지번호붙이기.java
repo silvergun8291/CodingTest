f8032d24a62c57e31216ff4caa71aa631398ead9
silvergun8291 <81484874+silvergun8291@users.noreply.github.com>
2025-03-11 20:36:33 +0900

    [Silver I] Title: 단지번호붙이기, Time: 108 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -3,64 +3,68 @@ import java.util.*;
 
 public class Main {
     static int N;
-    static int [][] graph;
+    static int [][] map;
     static boolean [][] visited;
-    static int [] dx = {-1, 0, 1, 0};
-    static int [] dy = {0, 1, 0, -1};
-    static List<Integer> result;
-    static int size;
+    static int [] dx = {0, 0, -1, 1};
+    static int [] dy = {-1, 1, 0, 0};
+    static List<Integer> apart;
 
-    static boolean boundCheck(int y, int x) {
-        return x >= 0 && x < N && y >= 0 && y < N;
-    }
+    static void bfs(int y, int x) {
+        Queue<int []> queue = new ArrayDeque<>();
+        queue.add(new int [] {y, x});
+        visited[y][x] = true;
+        int count = 1;
 
-    static void DFS(int currentY, int currentX) {
-        size++;
-        visited[currentY][currentX] = true;
+        while (!queue.isEmpty()) {
+            int [] cur = queue.poll();
+            int curY = cur[0];
+            int curX = cur[1];
 
-        for (int i = 0; i < 4; i++) {
-            int newX = currentX + dx[i];
-            int newY = currentY + dy[i];
+            for (int d = 0; d < 4; d++) {
+                int nextY = curY + dx[d];
+                int nextX = curX + dy[d];
 
-            if (boundCheck(newY, newX) && graph[newY][newX] == 1 && !visited[newY][newX]) {
-                DFS(newY, newX);
-            }
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());    // 지도의 크기를 입력받음
-        graph = new int[N][N];
-
-        // 지도를 입력받음
-        for (int i = 0; i < N; i++) {
-            String line = br.readLine();
-
-            for (int j = 0; j < N; j++) {
-                graph[i][j] = Character.getNumericValue(line.charAt(j));
-            }
-        }
-
-        visited =new boolean[N][N];        // visited 배열을 초기화
-        result = new ArrayList<>();      // result 변수를 초기화
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (graph[i][j] == 1 && !visited[i][j]) {
-                    size = 0;
-                    DFS(i, j);
-                    result.add(size);
+                if (nextY >= 0 && nextY < N && nextX >= 0 && nextX < N && !visited[nextY][nextX] && map[nextY][nextX] == 1) {
+                    visited[nextY][nextX] = true;
+                    queue.add(new int [] {nextY, nextX});
+                    count++;
                 }
             }
         }
 
-        Collections.sort(result);
+        apart.add(count);
+    }
 
-        System.out.println(result.size());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
 
-        for (Integer integer : result) {
-            System.out.println(integer);
+        map = new int[N][N];
+        visited = new boolean[N][N];
+        apart = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            char [] row = br.readLine().toCharArray();
+
+            for (int j = 0; j < N; j++) {
+                map[i][j] = row[j] - '0';
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (!visited[i][j] && map[i][j] == 1) {
+                    bfs(i, j);
+                }
+            }
+        }
+
+        System.out.println(apart.size());
+
+        Collections.sort(apart);
+
+        for (int n: apart) {
+            System.out.println(n);
         }
     }
 }
